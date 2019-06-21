fd6c641dda9b303fc03a94be533c272d8ca44063
bugzweiser <bugzweiser@gmail.com>
2019-06-21 13:52:06 -0700

    concert and movie search completed

--- liri.js
+++ liri.js
@@ -10,22 +10,23 @@ moment().format();
 
 //search processes
 if (process.argv[2] === "concert-this") {
-    // console.log("you asked for a concert");
+    console.log("You Asked For Concert Info");
 
     var input = process.argv[3];
-    var bandsUrl = "https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp"
+    var bandsUrl = "https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp";
     bands(bandsUrl);
 }
 if (process.argv[2] === "spotify-this-song") {
-    console.log("you asked for spotify");
+    console.log("You Asked For Spotify");
 
     // var input = process.argv[3];
     // var bandsUrl = "https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp"
 }
 if (process.argv[2] === "movie-this") {
-    console.log("you asked for a movie");
-    // var input = process.argv[3];
-    // var bandsUrl = "https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp"
+    console.log("You Asked For A Movie");
+    var input = process.argv[3];
+    var movieUrl = "http://www.omdbapi.com/?t=" + input + "&y=&plot=short&apikey=trilogy";;
+    movie(movieUrl);
 }
 
 //bands in town call
@@ -43,11 +44,10 @@ function bands(bandsUrl) {
                 var showDateRtn = moment(showDate).format("MM/DD/YYYY");
 
                 console.log("--------------Event: " + [i + 1] + "---------------");
-                console.log("Venue Name: " + bandsResponse.data[i].venue.name);
                 console.log("Location: " + bandsResponse.data[i].venue.city);
-                // console.log("Date: " + bandsResponse.data[i].datetime);
-                console.log("Location: " + showDateRtn);
-                console.log("Tickets: " + bandsResponse.data[i].url);
+                console.log("Venue Name: " + bandsResponse.data[i].venue.name);
+                console.log("Event Date: " + showDateRtn);
+                console.log("Tickets URL: " + bandsResponse.data[i].url);
                 console.log("-------------------------------------");
             }
         }
@@ -81,17 +81,20 @@ function movie(movieUrl) {
         function(movieResponse) {
             // console.log(bandsResponse);
 
-            var searchLimit = 10;
+            var movieDate = movieResponse.data.Released
+            var movieDateRtn = moment(movieDate, "DDMMMYYYY").format("MM/DD/YYYY");
 
-            for (i = 0; i < searchLimit; i++) {
+            console.log("------------------Info------------------");
+            console.log("Movie Title: " + movieResponse.data.Title);
+            console.log("Release Date: " + movieDateRtn);
+            console.log("IMDB Rating: " + movieResponse.data.imdbRating);
+            console.log("Rotton Tomatoes Rating: " + movieResponse.data.Ratings[1].Value);
+            console.log("Country Produced: " + movieResponse.data.Country);
+            console.log("Language: " + movieResponse.data.Language);
+            console.log("Plot: " + movieResponse.data.Plot);
+            console.log("Actors: " + movieResponse.data.Actors);
+            console.log("----------------------------------------");
 
-                console.log("--------------Event: " + [i + 1] + "---------------");
-                console.log("Venue Name: " + bandsResponse.data[i].venue.name);
-                console.log("Location: " + bandsResponse.data[i].venue.city);
-                console.log("Date: " + bandsResponse.data[i].datetime);
-                console.log("Tickets: " + bandsResponse.data[i].url);
-                console.log("-------------------------------------");
-            }
         }
     );
 }
